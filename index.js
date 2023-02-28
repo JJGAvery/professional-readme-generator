ddc6846d94260146b0e8cc43232d7c2206785c33
JJGAvery <116852587+JJGAvery@users.noreply.github.com>
2023-02-27 18:26:12 -0700

    added packages needed for application. added questions array.

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'projectTitle',
+    message: 'What is your projects title?',
+  },
+  {
+    type: 'input',
+    name: 'projectDescription',
+    message: 'Please enter a short description of your project.',
+  },
+  {
+    type: 'input',
+    name: 'installationSteps',
+    message: 'What command should you enter to install dependencies?',
+  },
+  {
+    type: 'input',
+    name: 'projectUse',
+    message: 'What should the user need to know about using your repository?',
+  },
+  {
+    type: 'input',
+    name: 'projectContribute',
+    message: 'What should the user need to know about contributing to your repository?',
+  },
+  {
+    type: 'input',
+    name: 'projectTest',
+    message: 'What command should be used to run tests?',
+  },
+  {
+    type: 'input',
+    name: 'projectLicense',
+    message: 'What license should your project have?',
+  },
+  {
+    type: 'input',
+    name: 'userName',
+    message: 'What is your github user name?',
+  },
+  {
+    type: 'input',
+    name: 'userEmail',
+    message: 'What is your email address?',
+  },
+])
+  
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
